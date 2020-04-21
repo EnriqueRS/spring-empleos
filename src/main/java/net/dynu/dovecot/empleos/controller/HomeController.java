f1b2eea14d5cbdc3519d2df0325f97d8a5400172
EnriqueRS <enrique.rios.santos@gmail.com>
2020-04-21 08:57:16 +0200

    Thymeleaf added

--- src/main/java/net/dynu/dovecot/empleos/controller/HomeController.java
+++ src/main/java/net/dynu/dovecot/empleos/controller/HomeController.java
@@ -1,5 +1,7 @@
 package net.dynu.dovecot.empleos.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,19 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
-		model.addAttribute("mensaje", "Bienvenidos a Empleos APP");
+//		model.addAttribute("mensaje", "Bienvenidos a Empleos APP");
+//		model.addAttribute("date", new Date());
+		
+		String nombre = "Auxiliar de Contabilidad";
+		Date fechaPub = new Date();
+		double salario = 9000.0;
+		boolean vigente = true;
+		
+		model.addAttribute("nombre", nombre);
+		model.addAttribute("fecha", fechaPub);
+		model.addAttribute("salario", salario);
+		model.addAttribute("vigente", vigente);
+		
 		return "home";
 	}
 

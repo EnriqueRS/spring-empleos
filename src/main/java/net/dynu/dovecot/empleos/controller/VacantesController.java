118695cd52dd053bfbf8bb58a31736717e4ebdec
EnriqueRS <enrique.rios.santos@gmail.com>
2020-05-17 18:05:44 +0200

    footer added

--- src/main/java/net/dynu/dovecot/empleos/controller/VacantesController.java
+++ src/main/java/net/dynu/dovecot/empleos/controller/VacantesController.java
@@ -1,37 +1,55 @@
 package net.dynu.dovecot.empleos.controller;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import net.dynu.dovecot.empleos.model.Vacante;
 import net.dynu.dovecot.empleos.service.IVacantesService;
 
-
 @Controller
 @RequestMapping("/vacantes")
 public class VacantesController {
-	
+
 	@Autowired
 	private IVacantesService serviceVacantes;
-	
+
+	@GetMapping("/create")
+	private String crear() {
+		return "vacantes/formVacante";
+	}
+
+	@PostMapping("/save")
+	private String guardar(@RequestParam("nombre") String nombre,
+			@RequestParam("descripcion") String descripcion,
+			@RequestParam("categoria") String categoria,
+			@RequestParam("estatus") String estatus,
+			@RequestParam("fecha") String fecha,
+			@RequestParam("destacado") int destacado,
+			@RequestParam("salario") double salario) {
+		return "vacantes/listVacantes";
+	}
+
 	@GetMapping("/delete")
 	public String eliminar(@RequestParam("id") int idVacante, Model model) {
 		System.out.println(idVacante);
 		model.addAttribute("id", idVacante);
 		return "mensaje";
 	}
-	
+
 	@GetMapping("/view/{id}")
-	public String verDetalle (@PathVariable("id") int idVacante, Model model) {
+	public String verDetalle(@PathVariable("id") int idVacante, Model model) {
 		Vacante vacante = serviceVacantes.buscarPorId(idVacante);
 		System.out.println("Vacante: " + vacante);
 		model.addAttribute("vacante", vacante);
-		
+
 		return "detalle";
 	}
 
